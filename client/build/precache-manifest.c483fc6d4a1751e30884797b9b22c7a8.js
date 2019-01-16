7e5c86aa3cb3ac6dba1616346433f73d3b3a7123
kitlingmui <kitlingmui@gmail.com>
2019-01-15 21:13:40 -0800

    Add build

---
+++ client/build/precache-manifest.c483fc6d4a1751e30884797b9b22c7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65b76e7b76cad3e0bb8532e3d0560a40",
+    "url": "/static/media/bg1.65b76e7b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff65b6be35d000e14c20",
+    "url": "/static/js/main.ff65b6be.chunk.js"
+  },
+  {
+    "revision": "068f9c0e9b0296157e63",
+    "url": "/static/js/1.068f9c0e.chunk.js"
+  },
+  {
+    "revision": "ff65b6be35d000e14c20",
+    "url": "/static/css/main.fc727813.chunk.css"
+  },
+  {
+    "revision": "daed92bee2563fd4ca1900fa1a93c282",
+    "url": "/index.html"
+  }
+];
